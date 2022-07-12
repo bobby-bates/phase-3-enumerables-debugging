91ff8a3049546d72ebb32677e96407da68c34108
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-12 18:27:42 -0400

    completed assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,45 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+
+=begin
+  words = sentence.split
+  reversed_words = []
+
+  words.each do |word|
+    reversed_words << word.reverse
+  end
+  
+  reversed_words.join(" ")
+=end
+
+# Refactor:
+
+=begin
+  words = sentence.split
+
+  reversed_words = words.map do |word|
+    word.reverse
+  end
+
+  reversed_words.join(" ")
+=end
+
+# I said refactor!
+
+=begin
+  words = sentence.split
+
+  reversed_words = words.map(&:reverse)
+
+  reversed_words.join(" ")
+=end
+
+# I SAID REFACTOR!
+
+  sentence.split.map(&:reverse).join(' ')
+
+# ...perfection.
+end
+
+puts reverse_each_word('Hello there, and how are you?')
